6df0a8975ce24928b1b19e4898a1eb62a72fef2f
minjee <leeminjeeofficial@gamil.com>
2022-05-22 17:29:44 +0900

    메인윈도우 클래스 생성

--- BugTracker/mainwindow.cpp
+++ BugTracker/mainwindow.cpp
@@ -6,10 +6,42 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initialize();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initialize()
+{
+    switch (m_eCurrentAuthority)
+    {
+        case Authority::ADMIN:      initialzieAsAdmin();        break;
+        case Authority::MANAGER:    initialzieAsManager();      break;
+        case Authority::DEVELOPER:  initialzieAsDeveloper();    break;
+        case Authority::TESTER:     initialzieAsTester();       break;
+    }
+}
+
+void MainWindow::initialzieAsAdmin()
+{
+
+}
+
+void MainWindow::initialzieAsManager()
+{
+
+}
+
+void MainWindow::initialzieAsDeveloper()
+{
+
+}
+
+void MainWindow::initialzieAsTester()
+{
+
 }
 
--- BugTracker/mainwindow.h
+++ BugTracker/mainwindow.h
@@ -7,15 +7,56 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
+/*!
+ * \brief The MainWindow class
+ * \details MainWindow for Bug Tracker Program
+ * \author Lee Minjee
+ * \date 2022-05-22
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief The Authority enum
+     * \details
+     */
+    enum Authority
+    {
+        ADMIN = 0,
+        MANAGER,
+        DEVELOPER,
+        TESTER
+    };
+
+    /*!
+     * \brief MainWindow
+     * \param parent nullptr
+     */
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
 private:
     Ui::MainWindow *ui;
+    /*!
+     * \brief   m_eCurrentAuthority
+     * \details
+     */
+    enum Authority m_eCurrentAuthority = ADMIN;
+
+private:
+    /*!
+     * \brief initialize
+     * \details initialize mainwindow class
+     */
+    void initialize();
+    void initialzieAsAdmin();
+    void initialzieAsManager();
+    void initialzieAsDeveloper();
+    void initialzieAsTester();
+
 };
 #endif // MAINWINDOW_H
