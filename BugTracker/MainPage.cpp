99503b2faaec35078f07b943b4b60df9178d8f12
minjee <leeminjeeofficial@gamil.com>
2022-05-23 21:07:15 +0900

    메인 화면 기본 UI 배치완료

--- BugTracker/MainPage.cpp
+++ BugTracker/MainPage.cpp
@@ -12,3 +12,65 @@ MainPage::~MainPage()
 {
     delete ui;
 }
+
+void MainPage::initializeAsAdmin()
+{
+    ui->stackedWidget->setCurrentIndex(HOME);
+}
+
+void MainPage::initializeAsManager()
+{
+    ui->stackedWidget->setCurrentIndex(HOME);
+    ui->pb_manage_users->hide();
+    ui->pb_manage_users->setEnabled(false);
+}
+
+void MainPage::initializeAsDeveloper()
+{
+    ui->stackedWidget->setCurrentIndex(HOME);
+    ui->pb_manage_users->hide();
+    ui->pb_manage_users->setEnabled(false);
+}
+
+void MainPage::initializeAsTester()
+{
+    ui->stackedWidget->setCurrentIndex(HOME);
+    ui->pb_manage_users->hide();
+    ui->pb_manage_users->setEnabled(false);
+}
+
+void MainPage::on_pb_home_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(HOME);
+}
+
+
+void MainPage::on_pb_manage_users_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(MANAGE_USERS);
+}
+
+
+void MainPage::on_pb_my_projects_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(MY_PROJECT);
+}
+
+
+void MainPage::on_pb_my_tickets_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(MY_TICKET);
+}
+
+
+void MainPage::on_pb_Profile_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(PROFILE);
+}
+
+
+void MainPage::on_pb_logout_clicked()
+{
+    emit logout();
+}
+
--- BugTracker/MainPage.h
+++ BugTracker/MainPage.h
@@ -15,8 +15,43 @@ public:
     explicit MainPage(QWidget *parent = nullptr);
     ~MainPage();
 
+    void initializeAsAdmin();
+    void initializeAsManager();
+    void initializeAsDeveloper();
+    void initializeAsTester();
+
+signals:
+    void logout();
+
+private slots:
+    void on_pb_home_clicked();
+
+    void on_pb_manage_users_clicked();
+
+    void on_pb_my_projects_clicked();
+
+    void on_pb_my_tickets_clicked();
+
+    void on_pb_Profile_clicked();
+
+    void on_pb_logout_clicked();
+
+private:
+    enum PAGE_INDEX
+    {
+        HOME = 0,
+        MANAGE_USERS,
+        MY_PROJECT,
+        MY_TICKET,
+        PROFILE
+    };
+
 private:
     Ui::MainPage *ui;
+
+
+
+
 };
 
 #endif // MAINPAGE_H
